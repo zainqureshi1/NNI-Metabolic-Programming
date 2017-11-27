124c88d716b21a690da34d577735813c9dda33b2
zainqureshi1 <zain.qureshi.ly@gmail.com>
2017-11-27 18:18:13 +0500

    Updated sound and music
    Changed background music and better handling of volume of different
    sounds and music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,18 +12,21 @@ public class SoundManager : MonoBehaviour {
 
 	public void PlayButtonClick() {
 		soundSource.Stop ();
+		soundSource.volume = 1.0f;
 		soundSource.clip = buttonClick;
 		soundSource.Play ();
 	}
 
 	public void PlayCorrect() {
 		soundSource.Stop ();
+		soundSource.volume = 1.0f;
 		soundSource.clip = correct;
 		soundSource.Play ();
 	}
 
 	public void PlayWrong() {
 		soundSource.Stop ();
+		soundSource.volume = 0.4f;
 		soundSource.clip = wrong;
 		soundSource.Play ();
 	}
